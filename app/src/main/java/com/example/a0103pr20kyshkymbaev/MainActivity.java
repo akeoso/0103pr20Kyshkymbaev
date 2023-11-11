f48b9681134c2a6062e2e71e6b34b91029c38c9c
Kyshkymbaev <roma.akumov@bk.ru>
2023-11-11 23:09:50 +0500

    Initial commit

---
+++ app/src/main/java/com/example/a0103pr20kyshkymbaev/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a0103pr20kyshkymbaev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "0103pr20Kyshkymbaev"
+include(":app")
+ 
